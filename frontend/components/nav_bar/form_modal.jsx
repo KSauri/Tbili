1f55a39024b741e441020fa5bbd3d692c68e8dc0
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-02-14 22:12:07 -0500

    Add signup form logic

--- frontend/components/nav_bar/form_modal.jsx
+++ frontend/components/nav_bar/form_modal.jsx
@@ -3,58 +3,45 @@ import SignUpContainer from '../auth_forms/sign_up_container';
 import React, { Component } from 'react';
 
 
-export default function ({ showLogIn, showSignUp }) {
-  if (showLogIn)
-   { return (
-    <div className='modal-screen'>
-      <LogInContainer />
-    </div>
-   ); }
-  else if (showSignUp)
-  { return (
-    <div className='modal-screen'>
-      <SignUpContainer className='modal-content'/>
-    </div>
-  ); }
-  else { return null; }
-}
-
-// export default class FormModal extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       logInClass: this.props.showLogIn ? "modal-screen" : "modal-screen turn-off",
-//       signUpClass: this.props.showSignUp ? "modal-screen" : "modal-screen turn-off"
-//     };
-//   }
-//
-//   componentDidMount() {
-//     if (this.props.showLogIn)
-//      { this.form_modal = (
-//       <div className='modal-screen'>
-//         <LogInContainer />
-//       </div>
-//      ); }
-//     else if (this.props.showSignUp)
-//     { this.form_modal = (
-//       <div className='modal-screen'>
-//         <SignUpContainer className='modal-content'/>
-//       </div>
-//     ); }
-//     else { this.form_modal = null; }
-//   }
-//
-//   render() {
-//     debugger
-//     return(
-//       <div>
-//         <div className={ this.state.logInClass }>
-//           <LogInContainer className='modal-content'/>
-//         </div>
-//         <div className={ this.state.logInClass }>
-//           <SignUpContainer className='modal-content'/>
-//         </div>
-//       </div>
-//     );
-//   }
+// export default function ({ showLogIn, showSignUp }) {
+//   if (showLogIn)
+//    { return (
+//     <div className='modal-screen'>
+//       <LogInContainer />
+//     </div>
+//    ); }
+//   else if (showSignUp)
+//   { return (
+//     <div className='modal-screen'>
+//       <SignUpContainer className='modal-content'/>
+//     </div>
+//   ); }
+//   else { return null; }
 // }
+
+export default class FormModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logIn: this.props.showLogIn,
+      signUpClass: this.props.showSignUp
+    };
+  }
+
+
+  render() {
+    if (this.state.logIn) {
+      return (
+      <div className="modal-screen">
+        <LogInContainer className='modal-content'/>
+      </div>);
+    } else if (this.state.signUpClass) {
+      return (
+      <div className={ this.state.logInClass }>
+        <SignUpContainer className='modal-content'/>
+      </div>);
+    } else {
+      return null;
+    }
+  }
+}
