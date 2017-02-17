ff828d0fb8f7f57201f2e2875edffc930536a7f3
Kyle Sauri <kylesauri@gmail.com>
2017-02-16 22:01:51 -0500

    Add rheostat

--- frontend/components/spots/spots.jsx
+++ frontend/components/spots/spots.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import Rheostat from 'rheostat';
+
 
 const Spots = () => {
-  return (<div>testing</div>);
+  return (<div>
+    <Rheostat className="rheostat"
+      min={1}
+      max={100}
+      values={[1, 100]}
+      />
+    </div>);
 };
 
 export default Spots;
