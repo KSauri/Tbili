f8790ca516fd7f7e474ea55fa635d90dbc7ab48b
iserbit <appacademy@Apps-Mac-mini-2.local>
2017-02-17 09:19:16 -0500

    Update seeds

--- frontend/components/featured_spots/featured_non_homes_container.js
+++ frontend/components/featured_spots/featured_non_homes_container.js
@@ -3,11 +3,14 @@ import FeaturedMiddleLayer from './featured_middle_layer';
 import * as selector from '../../reducers/selectors';
 import { fetchFeaturedSpots } from '../../actions/spot_actions';
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state) => {
+
+  return({
   non_home_spots: selector.selectFeaturedNonHomes(state),
   home_spots: selector.selectFeaturedHomes(state),
   fancy_home_spots: selector.selectFeaturedFancyHomes(state)
-});
+  });
+};
 
 const mapDispatchToProps = dispatch => ({
   fetchFeaturedSpots: () => dispatch(fetchFeaturedSpots()),
