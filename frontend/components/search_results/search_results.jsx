5981982ec95bb583ec87eb8dc8ad4b07bc5dafa1
Kyle Sauri <kylesauri@gmail.com>
2017-02-19 11:43:03 -0500

    Add styling to search results page

--- frontend/components/search_results/search_results.jsx
+++ frontend/components/search_results/search_results.jsx
@@ -3,9 +3,9 @@ import SearchMapContainer from './map/map_container';
 import SearchResultIndexContainer from './search_result_index/search_result_index_container';
 
 const SearchResults = () => {
-  return (<div>
-    <SearchMapContainer />
+  return (<div className="search-result-outer-container">
     <SearchResultIndexContainer />
+    <SearchMapContainer className="map-container" />
   </div>);
 };
 
