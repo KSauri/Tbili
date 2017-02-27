9f58d624e001bb240aa387d9927222564391fbca
Kyle Sauri <kylesauri@gmail.com>
2017-02-27 11:42:49 -0500

    Fixed issue with webpack config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -24,7 +24,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']
         }
